db9d2ada1cf8c11fb4c1f4961c04d61765531393
Alexander Buysse <alexanderbuysse002@gmail.com>
2021-05-02 17:04:33 +0200

    help

--- persoonlijk-frontend/pages/cards/[slug].js
+++ persoonlijk-frontend/pages/cards/[slug].js
@@ -2,6 +2,8 @@ import Layout from "../../components/Layout";
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/router";
+import styles from "../../components/AddCard.module.css";
+
 
 const Card = ({ data }) => {
   const router = useRouter();
@@ -12,11 +14,34 @@ const Card = ({ data }) => {
   return (
     <Layout>
       <>
+      <section>
+      <h3>Message For Home</h3>
+      <form className={styles.form}>
+          <label className={styles.label}>
+            Location:
+            <input className="location" type="text" name="location" value={data.location} readOnly required />
+          </label>
+
+        <label className={styles.label}>
+          From:
+          <input type="text" value={data.from} name="from" required />
+        </label>
+        <label className={styles.label}>
+          To:
+          <input type="text" value={data.to} name="to" required />
+        </label>
+        <label className={styles.label}>
+          Message:
+          <textarea name="content" required maxLength="500" value={data.message}></textarea>
+        </label>
+      </form>
+      <p>Last post card: {process.env.LINK}/cards/{data.slug}</p>
+      </section>
         <h2>{data.from}</h2>
         <p>{data.to}</p>
         <p>{data.location}</p>
-        <p>{data.location}</p>
         <p>{data.message}</p>
+        <p>{data.weather}</p>
         <img src={data.src} alt={data.Imgdescript} width="200"></img>
       </>
     </Layout>
